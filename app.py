6b74fea518ab58f6eb050675d97dbaac130fb985
talli04 <tallitacris28@gmail.com>
2025-03-23 21:44:15 -0300

    atualização

--- app.py
+++ app.py
@@ -1,23 +1,44 @@
-from flask import Flask
+from flask import Flask,request,jsonify
+
+import sqlite3
 
 app = Flask(__name__)
 
 #route -> são os endpoints da nossa API
 @app.route("/")
-def pagar_pessoas():
+def Api_desafio():
 
-    return "<h1>Começar a semana, pagando suas dívidas, é bom demais</h1>"
+    return "<h1>API que vai ser utilizada para o desafio 2 </h1>"
 
-@app.route("/pix")
-def mande_o_pix():
+def init_db():
 
-    return "<h3>Pagar as pessoas faz bem pras pessoas!!! =D</h3>"
+    with sqlite3.connect("database.db") as conn:
+        conn.execute(
+            """
+                CREATE TABLE IF NOT EXISTS LIVROS(
+                    id INTEGER PRIMARY KEY AUTOINCREMENT,
+                    titulo TEXT NOT NULL,
+                    categoria TEXT NOT NULL,
+                    autor TEXT NOT NULL,
+                    imagem_url TEXT NOT NULL
+                )
+            """
+        )
 
-@app.route("/comidas")
-def prato_do_dia():
+init_db()
 
-    return "<h1>o prato do dia é feijoada</h1>"
+@app.route("/doar", methods=["POST"])
+def doar():
 
+    dados = request.get_json()
+
+    titulo = dados.get("titulo")
+    categoria = dados.get("categoria")
+    autor = dados.get("autor")
+    imagem_url = dados.get("imagem_url")
+
+    if not titulo or not categoria or not autor or not imagem_url:
+        return jsonify({"erro":"Todos os campos são obrigatórios"}),400
 
 # se o arquivo app.py == ao arquivo principal da nossa aplicação
 if __name__ == "__main__":
